52718d38ae9205844ff898e8072b602e5d83971e
ajac-zero <ajcardoza2000@gmail.com>
2024-11-04 18:06:01 -0600

    added more recorder tests

---
+++ tests/test_recorder.py
@@ -0,0 +1,47 @@
+import pytest
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from memento import crud, recorder, models
+
+
+engine = create_engine("sqlite://")
+
+models.Base.metadata.create_all(engine)
+
+
+@pytest.fixture
+def session():
+    with Session(engine) as s:
+        yield s
+
+
+def test_create_conversation(session):
+    c = crud.create_conversation(session, "Sarahbot")
+
+    assert isinstance(c, models.Conversation)
+    assert c.agent == "Sarahbot"
+
+
+@pytest.fixture
+def re(session):
+    return recorder.Recoder.from_conversation(session, 1)
+
+
+def test_add_message(re):
+    x = len(re.messages)
+    y = len(re.new_messages)
+
+    re.add_message(role="system", content="hello!")
+
+    assert len(re.messages) == (x + 1)
+    assert len(re.new_messages) == (y + 1)
+
+
+def test_commit_messages(session, re):
+    re.add_message(role="system", content="You're an awesome robot")
+    re.add_message(role="user", content="hello!")
+    message_ids = re.commit_new_messages(session)
+
+    for n, id in enumerate(message_ids, start=1):
+        assert id == n

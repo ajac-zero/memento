399c7f08069d95e42962e7b8ccfa7fc2a23b890c
ajac-zero <ajcardoza2000@gmail.com>
2024-11-05 20:37:51 -0600

    added cli to run migrations

--- memento/cli.py
+++ memento/cli.py
@@ -1,18 +1,26 @@
 import os
+
 import click
+from alembic import command, config
 from rich import print
 from rich.prompt import Prompt
+
+ENV_VAR = "MEMENTO_DB_CONNECTION_STRING"
+CONFIG_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 @click.command()
 def cli():
-    db_url = os.getenv("MEMENTO_DB_URL", None)
+    alembic_cfg = config.Config(f"{CONFIG_DIR}/alembic.ini")
+    alembic_cfg.set_main_option("script_location", f"{CONFIG_DIR}/migrations")
 
-    if db_url is None:
-        print("No connection string found at environment variable `MEMENTO_DB_URL`.")
-        db_url = Prompt.ask("Enter your database connection string")
+    if os.getenv(ENV_VAR, None) is None:
+        print(f"No connection string found at environment variable {ENV_VAR}.")
+        os.environ[ENV_VAR] = Prompt.ask("Enter your database connection string")
+    else:
+        print(f"MEMENTO_DB_CONNECTION_STRING found.")
 
-    print(db_url)
+    command.upgrade(alembic_cfg, "head")
 
 
 if __name__ == "__main__":

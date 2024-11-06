5e058c4e08f76d914bf686f64a0e3ca0e5ff2d0a
ajac-zero <ajcardoza2000@gmail.com>
2024-11-05 21:07:12 -0600

    added pretty cli

--- memento/cli.py
+++ memento/cli.py
@@ -2,7 +2,7 @@ import os
 
 import click
 from alembic import command, config
-from rich import print
+from rich import print as rprint
 from rich.prompt import Prompt
 
 ENV_VAR = "MEMENTO_DB_CONNECTION_STRING"
@@ -10,18 +10,26 @@ CONFIG_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 @click.command()
-def cli():
+@click.argument("action")
+def cli(action):
+    """Run a Memento migration. Action can be "upgrade" to create tables or "downgrade" to destroy them."""
     alembic_cfg = config.Config(f"{CONFIG_DIR}/alembic.ini")
     alembic_cfg.set_main_option("script_location", f"{CONFIG_DIR}/migrations")
 
     if os.getenv(ENV_VAR, None) is None:
-        print(f"No connection string found at environment variable {ENV_VAR}.")
-        os.environ[ENV_VAR] = Prompt.ask("Enter your database connection string")
+        rprint(
+            "[red]No connection string found at environment variable:[/]",
+            f"[green]{ENV_VAR}[/].",
+        )
+        os.environ[ENV_VAR] = Prompt.ask(
+            "[bold cyan]Please enter your database connection string[/]"
+        )
     else:
-        print(f"MEMENTO_DB_CONNECTION_STRING found.")
+        rprint(f"[green]{ENV_VAR}[/] [blue]environment variable found.[/]")
 
-    command.upgrade(alembic_cfg, "head")
-
-
-if __name__ == "__main__":
-    cli()
+    if action == "upgrade":
+        command.upgrade(alembic_cfg, "head")
+    elif action == "downgrade":
+        command.downgrade(alembic_cfg, "base")
+    else:
+        raise ValueError("Invalid action.")

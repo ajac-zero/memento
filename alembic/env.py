05864cccbf1e5abe5739ac65f1735d7d141ab7a6
ajac-zero <ajcardoza2000@gmail.com>
2024-11-06 19:12:18 -0600

    moved alembic to root dir

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,9 @@
 import os
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from memento.models import Base
 
 config = context.config

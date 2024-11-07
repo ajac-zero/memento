adf91aa73dba96e5ab1a056a3145988f4fc2dfb3
ajac-zero <ajcardoza2000@gmail.com>
2024-11-06 19:21:05 -0600

    added echo option to cli

--- memento/cli.py
+++ memento/cli.py
@@ -11,7 +11,8 @@ MIGRATIONS_DIR = f"{os.path.dirname(os.path.realpath(__file__))}/migrations"
 
 @click.command()
 @click.argument("action")
-def cli(action):
+@click.option("--echo/--no-echo", default=False)
+def cli(action, echo):
     """Run a Memento migration. Action can be "upgrade" to create tables or "downgrade" to destroy them."""
 
     if action not in {"upgrade", "downgrade"}:
@@ -28,7 +29,7 @@ def cli(action):
     else:
         rprint(f"[green]{ENV_VAR}[/] [blue]environment variable found.[/]")
 
-    engine = create_engine(db_url, echo=True)
+    engine = create_engine(db_url, echo=echo)
 
     with open(f"{MIGRATIONS_DIR}/{action}.sql", "r") as file:
         queries = [query for query in file.read().split("\n\n")]
